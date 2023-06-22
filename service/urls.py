92848bb7765097796eefa61345783c3e94844578
Vlad Tetruk <vladteteruk01@gmail.com>
2023-06-22 15:11:44 +0300

    Implement cook_list.html / add urls & views for Cook model

--- service/urls.py
+++ service/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from service.views import IndexView
+from service.views import IndexView, CookViewList
 
 urlpatterns = [
-    path("", IndexView.as_view(), name="index")
+    path("", IndexView.as_view(), name="index"),
+    path("cook/", CookViewList.as_view(), name="cooks-list"),
 ]
 
 app_name = "service"
--- service/views.py
+++ service/views.py
@@ -1,5 +1,11 @@
+from django.contrib.auth import get_user_model
 from django.views import generic
 
 
 class IndexView(generic.TemplateView):
     template_name = "service/index.html"
+
+
+class CookViewList(generic.ListView):
+    model = get_user_model()
+    template_name = "service/cook_list.html"
