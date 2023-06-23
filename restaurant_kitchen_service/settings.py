794c7f823b41727cc7ec953a9f2cef7ed80101e7
Vlad Tetruk <vladteteruk01@gmail.com>
2023-06-23 15:16:36 +0300

    Implement LOGIN_REDIRECT_URL in settings

--- restaurant_kitchen_service/settings.py
+++ restaurant_kitchen_service/settings.py
@@ -103,6 +103,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 AUTH_USER_MODEL = "service.Cook"
 
+LOGIN_REDIRECT_URL = "service:index"
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
