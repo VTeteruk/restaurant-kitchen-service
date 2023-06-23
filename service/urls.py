e6bbdeed33f8db2909981991b6ad698b5f4998a0
Vlad Tetruk <vladteteruk01@gmail.com>
2023-06-23 15:01:18 +0300

    Add new view for creating users(cooks)

--- service/urls.py
+++ service/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from service.views import IndexView, CookListView, DishTypeListView, DishListView
+from service.views import IndexView,CookListView, DishTypeListView, DishListView, CookCreateView
 
 urlpatterns = [
     path("", IndexView.as_view(), name="index"),
     path("cook/", CookListView.as_view(), name="cooks-list"),
+    path("cook/create/", CookCreateView.as_view(), name="cook-create"),
     path("dish_type/", DishTypeListView.as_view(), name="dish-types-list"),
     path("dish/", DishListView.as_view(), name="dishes-list"),
 ]
