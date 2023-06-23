71a26804216c825965e7a83078170b58464cdf3c
Vlad Tetruk <vladteteruk01@gmail.com>
2023-06-23 15:00:38 +0300

    Create forms.py with UserCreationForm inside

---
+++ service/forms.py
@@ -0,0 +1,12 @@
+from django.contrib.auth.forms import UserCreationForm
+from service.models import Cook
+
+
+class CookForm(UserCreationForm):
+    class Meta(UserCreationForm.Meta):
+        model = Cook
+        fields = UserCreationForm.Meta.fields + (
+            "first_name",
+            "last_name",
+            "years_of_experience"
+        )
