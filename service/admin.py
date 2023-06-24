4a4d15ff7bc2363faadb0f3d5cf6b8ade401cda5
Vlad Tetruk <vladteteruk01@gmail.com>
2023-06-24 16:35:17 +0300

    Registrate models: Cook Dish DishType

--- service/admin.py
+++ service/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from django.contrib.auth import get_user_model
 
-# Register your models here.
+from service.models import Dish, DishType
+
+admin.site.register(get_user_model())
+admin.site.register(Dish)
+admin.site.register(DishType)
