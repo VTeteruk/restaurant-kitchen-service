dded2ca0c5cde717f7740af5879fd3614891bd7b
Vlad Tetruk <vladteteruk01@gmail.com>
2023-06-24 12:40:01 +0300

    Add placeholders for Cook forms

--- service/forms.py
+++ service/forms.py
@@ -10,3 +10,18 @@ class CookForm(UserCreationForm):
             "last_name",
             "years_of_experience"
         )
+
+    def __init__(self, *args, **kwargs) -> None:
+        super(CookForm, self).__init__(*args, **kwargs)
+        self.fields['username'].widget.attrs[
+            'placeholder'] = 'Enter your username'
+        self.fields['password1'].widget.attrs[
+            'placeholder'] = 'Enter your password'
+        self.fields['password2'].widget.attrs[
+            'placeholder'] = 'Confirm your password'
+        self.fields['first_name'].widget.attrs[
+            'placeholder'] = 'Enter your first name'
+        self.fields['last_name'].widget.attrs[
+            'placeholder'] = 'Enter your last name'
+        self.fields['years_of_experience'].widget.attrs[
+            'placeholder'] = 'Enter your years of experience'
